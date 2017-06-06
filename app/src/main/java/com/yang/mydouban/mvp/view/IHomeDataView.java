a2623e198af43ba538cd3f8dd1229c8c69c243c0
fengzhaoyang_i <fengzhaoyang_i@didichuxing.com>
2017-06-06 19:49:53 +0800

    简单提交

---
+++ app/src/main/java/com/yang/mydouban/mvp/view/IHomeDataView.java
@@ -0,0 +1,15 @@
+package com.yang.mydouban.mvp.view;
+
+/**
+ * Created by fengzhaoyang_i on 2017/6/6.
+ */
+
+public interface IHomeDataView {
+    void showMessage(String msg);
+
+    void showProgress();
+
+    void hideProgress();
+
+    void refreshData(Object result);
+}
